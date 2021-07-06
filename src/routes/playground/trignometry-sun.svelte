41800f3e4c887ebb42f9ec955b35374c986df586
Harpal Singh1 <harpalnitk1@gmail.com>
2021-07-06 10:17:10 +0530

    Added shapes using trignometry

---
+++ src/routes/playground/trignometry-sun.svelte
@@ -0,0 +1,15 @@
+<script>
+    import BasicPage from '$lib/components/UI/BasicPage.svelte';
+</script>
+<BasicPage title='Shapes using Trignometry'></BasicPage>
+
+
+<main>
+
+</main>
+
+
+
+<style lang="scss">
+	@import '../../styles/vars';
+</style>

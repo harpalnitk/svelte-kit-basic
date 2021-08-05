674d533361d771c4b3866da90321cefe95f81c7b
Harpal Singh1 <harpalnitk1@gmail.com>
2021-08-05 11:56:46 +0530

    added css properties

---
+++ src/routes/playground/3d-stack-cards.svelte
@@ -0,0 +1,21 @@
+<script>
+    import BasicPage from '$lib/components/UI/BasicPage.svelte';
+</script>
+<BasicPage title='3D Stack of Cards'></BasicPage>
+
+
+<main>
+
+</main>
+
+
+
+<style lang="scss">
+	@import '../../styles/vars';
+    main{
+    min-height: 100vh;
+    position: relative;
+    width: min(70rem, 90vw);
+    margin: 0 auto;
+}
+</style>
--- src/routes/playground/index.svelte
+++ src/routes/playground/index.svelte
@@ -1,91 +1,91 @@
 <script>
-    import BasicPage from '$lib/components/UI/BasicPage.svelte';
+	import BasicPage from '$lib/components/UI/BasicPage.svelte';
 </script>
-<BasicPage title='Playground for CSS' path='/'></BasicPage>
+
+<BasicPage title="Playground for CSS" path="/" />
 <main>
-    <h1>Playground</h1>
-    <ul class='link-grid'>
-        <li>1. <a href="/playground/http-firebase">Http Firebase</a></li>
-        <li>2. <a href="/playground/image-fragmentation">Image Fragmentation</a></li>
-        <li>3. <a href="/playground/spring">Spring Animation</a></li>
-        <li>4. <a href="/playground/svg-graphics">SVG Graphics</a></li>
-        <li>5. <a href="/playground/tweened">Tweened</a></li>
-        <li>6. <a href="/playground/css-hexagon">CSS Hexagon</a></li>
-        <li>7. <a href="/playground/box-shadow">Box Shadow Designs</a></li>
-        <li>8. <a href="/playground/clip-path-animations">Clip Path Animations</a></li>
-        <li>9. <a href="/playground/trignometry-sun">Shapes Using Trignometry</a></li>
-        <li>10. <a href="/playground/star-rating">Scalable Star rating</a></li>
-        <li>11. <a href="/playground/overlapping-cards">Overlapping Cards</a></li>
-        <li>12. <a href="/playground/glow-card">Double color neon glow card</a></li>
-        <li>13. <a href="/playground/curved-menu">Curved Outside Sidebar Menu</a></li>
-        <li>14. <a href="/playground/conic-spinner">Conic Gradient Spinner</a></li>
-        <li>15. <a href="/playground/glass">Transparent Cards-Glasmorphism</a></li>
-        <li>16. <a href="/playground/ripple-button">Ripple Button</a></li>
-        <li>17. <a href="/playground/product-card">Product card</a></li>
-        <li>18. <a href="/playground/creative-menu">Creative Menu</a></li>
-        <li>19. <a href="/playground/svg-animation">SVG Animation</a></li>
-        <li>20. <a href="/playground/ripple-animation">Ripple-Animation</a></li>
-        <li>21. <a href="/playground/shadows">CSS Shadows</a></li>
-        <li>22. <a href="/playground/player-card">Player card</a></li>
-        <li>23. <a href="/playground/profile-card">Profile card</a></li>
-        <li>24. <a href="/playground/hovering-cards">Hovering cards</a></li>
-        <li>25. <a href="/playground/clock">Clock using CSS</a></li>
-        <li>26. <a href="/playground/wavy-text">Wavy Text</a></li>
-        <li>27. <a href="/playground/glowing-card">Glowing Card</a></li>
-        <li>28. <a href="/playground/water-drops">Water Drops</a></li>
-        <li>28. <a href="/playground/radio-glow">Neon Glowing Radio Butotns</a></li>
-        <li>29. <a href="/playground/scale-video-scroll">Scale Video/Image on scroll</a></li>
-        <li>30. <a href="/playground/particle">Particle Effect</a></li>
-        <li>31. <a href="/playground/calculator">Calculator</a></li>
-        <li>32. <a href="/playground/liquid">Liquid Animation Effect</a></li>
-        <li>33. <a href="/playground/rotating-images">Rotating Images on Mouse Move</a></li>
-        <li>34. <a href="/playground/3d-stack-cards">3D Stack of a Card</a></li>
-    </ul>
+	<h1>Playground</h1>
+	<ul class="link-grid">
+		<li>1. <a href="/playground/http-firebase">Http Firebase</a></li>
+		<li>2. <a href="/playground/image-fragmentation">Image Fragmentation</a></li>
+		<li>3. <a href="/playground/spring">Spring Animation</a></li>
+		<li>4. <a href="/playground/svg-graphics">SVG Graphics</a></li>
+		<li>5. <a href="/playground/tweened">Tweened</a></li>
+		<li>6. <a href="/playground/css-hexagon">CSS Hexagon</a></li>
+		<li>7. <a href="/playground/box-shadow">Box Shadow Designs</a></li>
+		<li>8. <a href="/playground/clip-path-animations">Clip Path Animations</a></li>
+		<li>9. <a href="/playground/trignometry-sun">Shapes Using Trignometry</a></li>
+		<li>10. <a href="/playground/star-rating">Scalable Star rating</a></li>
+		<li>11. <a href="/playground/overlapping-cards">Overlapping Cards</a></li>
+		<li>12. <a href="/playground/glow-card">Double color neon glow card</a></li>
+		<li>13. <a href="/playground/curved-menu">Curved Outside Sidebar Menu</a></li>
+		<li>14. <a href="/playground/conic-spinner">Conic Gradient Spinner</a></li>
+		<li>15. <a href="/playground/glass">Transparent Cards-Glasmorphism</a></li>
+		<li>16. <a href="/playground/ripple-button">Ripple Button</a></li>
+		<li>17. <a href="/playground/product-card">Product card</a></li>
+		<li>18. <a href="/playground/creative-menu">Creative Menu</a></li>
+		<li>19. <a href="/playground/svg-animation">SVG Animation</a></li>
+		<li>20. <a href="/playground/ripple-animation">Ripple-Animation</a></li>
+		<li>21. <a href="/playground/shadows">CSS Shadows</a></li>
+		<li>22. <a href="/playground/player-card">Player card</a></li>
+		<li>23. <a href="/playground/profile-card">Profile card</a></li>
+		<li>24. <a href="/playground/hovering-cards">Hovering cards</a></li>
+		<li>25. <a href="/playground/clock">Clock using CSS</a></li>
+		<li>26. <a href="/playground/wavy-text">Wavy Text</a></li>
+		<li>27. <a href="/playground/glowing-card">Glowing Card</a></li>
+		<li>28. <a href="/playground/water-drops">Water Drops</a></li>
+		<li>28. <a href="/playground/radio-glow">Neon Glowing Radio Butotns</a></li>
+		<li>29. <a href="/playground/scale-video-scroll">Scale Video/Image on scroll</a></li>
+		<li>30. <a href="/playground/particle">Particle Effect</a></li>
+		<li>31. <a href="/playground/calculator">Calculator</a></li>
+		<li>32. <a href="/playground/liquid">Liquid Animation Effect</a></li>
+		<li>33. <a href="/playground/rotating-images">Rotating Images on Mouse Move</a></li>
+		<li>34. <a href="/playground/3d-stack-cards">3D Stack of a Card</a></li>
+		<li>35. <a href="/playground/shape-outside">CSS Shape Outside Property</a></li>
+		<li>36. <a href="/playground/text-emphasis">CSS Text Emphasis Property</a></li>
+		<li>37. <a href="/playground/dialog-backdrop">Dialog with CSS Backdrop Property</a></li>
+	</ul>
 </main>
-
-
 
 <style lang="scss">
 	@import '../../styles/vars';
 
+	main {
+		padding: 1rem;
+		margin: 0 auto;
+		width: fit-content;
+	}
+	.link-grid li {
+		// display: flex;
+	}
+	.link-grid li a {
+		text-decoration: none;
+		// color: #a9a39f;
+		position: relative;
+		display: inline-block;
+		overflow: hidden;
+		margin-right: 0.2rem;
+	}
+	.link-grid li a:before {
+		content: '';
+		transition: 0.2s;
+		position: absolute;
+		top: calc(100% - 1px);
+		left: 0;
+		width: 100%;
+		height: 1px;
+		background: #333;
+		transform: translateX(-101%);
+	}
 
-    main {
-        padding: 1rem;
-        margin:0 auto;
-        width: fit-content;
-       
-    }
-    .link-grid li {
-   // display: flex;
-}
-    .link-grid li a {
-text-decoration: none;
-// color: #a9a39f;
-    position: relative;
-    display: inline-block;
-    overflow: hidden;
-    margin-right: .2rem;
-    }
-    .link-grid li a:before {
-    content: "";
-    transition: .2s;
-    position: absolute;
-    top: calc(100% - 1px);
-    left: 0;
-    width: 100%;
-    height: 1px;
-    background: #333;
-    transform: translateX(-101%);
-    }
+	.link-grid li a:focus,
+	.link-grid li a:hover {
+		// color: #fff;
+		opacity: 1;
+	}
 
-    .link-grid li a:focus, .link-grid li a:hover {
-    // color: #fff;
-    opacity: 1;
-}
-
-.link-grid li a:focus:before, .link-grid li a:hover:before {
-    transform: translateX(0);
-}
-  
-
+	.link-grid li a:focus:before,
+	.link-grid li a:hover:before {
+		transform: translateX(0);
+	}
 </style>
